b0eec93bf3829315a6a59d4db41368141fcf8017
Aryajain881 <jainarya811@gmail.com>
2026-04-23 21:00:28 +0530

    Add files via upload

---
+++ day81.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+
+   
+    for (int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
